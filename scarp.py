42d6194bbc61e8498d7d3687f189c4b78f7919b9
DLrig@wsl <lashi0812@gmail.com>
2023-08-04 10:49:04 +0530

    add date

--- scarp.py
+++ scarp.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     driver.get(args.link)
     WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH,'//*[@id="bib"]/ul/li')))
     references = driver.find_elements(By.XPATH,'//*[@id="bib"]/ul/li')
-    arxiv_dict = {}
+    results = []
     for ref in tqdm(references,desc="Fetching the arxiv link"):
         span = ref.find_elements(By.XPATH,"span")
         author = span[0].text.split(" ")[0]
@@ -31,22 +31,25 @@ if __name__ == "__main__":
         found = False;
         for result in search.results():
             if(span[2].text.lower()[:-5] in result.title.lower() ):
-                arxiv_dict[result.title.lower()] = result.entry_id
+                results.append(result)
                 found = True;
                 break;
         if not found:
             print(f"{query} not found")
         
-    df = pd.DataFrame(columns=["title","references","citations","links"])          
-    for idx,(title,link) in tqdm(enumerate(list(arxiv_dict.items())),desc="Searching for the code",total=len(list(arxiv_dict.items()))):
+    df = pd.DataFrame(columns=["title","published","updated","arxiv_link","references","citations","code_links"])          
+    for idx,result in tqdm(enumerate(results),desc="Searching for the code",total=len(results)):
         paper_dict = {}
         
         # paper_list["title"].append(result.title.lower())
-        paper_dict["title"] = title
+        paper_dict["title"] = result.title
+        paper_dict["published"] = result.published.date().isoformat()
+        paper_dict["updated"] = result.updated.date().isoformat()
+        paper_dict["arxiv_link"] = result.entry_id
         
         try:
         
-            driver.get(link)    
+            driver.get(result.entry_id)    
             
             WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="labstabs"]/div/label[1]')))
             ref_tab = driver.find_element(By.XPATH, '//*[@id="labstabs"]/div/label[1]')
@@ -78,19 +81,19 @@ if __name__ == "__main__":
                 pwc.click()
             
             try:
-                WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="pwc-output"]/p[*]/a ')))
+                WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="pwc-output"]/p[*]/a ')))
                 code_link = driver.find_elements(By.XPATH, '//*[@id="pwc-output"]/p[*]/a ')
                 link = []
                 for ele in code_link:
                     # paper_list.append((ele.get_attribute("href")))
                     link.append(ele.get_attribute("href"))
-                paper_dict["links"]=link
+                paper_dict["code_links"]= link[0] if len(link) == 1 else link
                 
             except:
-                paper_dict["links"] = None
-                print(f"{title} code not found")
+                paper_dict["code_links"] = None
+                print(f"{paper_dict['title']} code not found")
                 
             df.loc[idx] = paper_dict
         except:
-            print(f"Skipping {title}...")
-    df.to_csv("out.csv")
+            print(f"Skipping {paper_dict['title']}...")
+    df.to_csv("out.csv",index=False)
